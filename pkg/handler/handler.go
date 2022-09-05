39cec32bfac8c0cd6d73b3eefcae3d49a3b0978d
Shawn O'Dell <sodell@gmail.com>
2022-09-05 16:56:28 -0500

    init

---
+++ pkg/handler/handler.go
@@ -0,0 +1,62 @@
+/*
+The MIT License (MIT)
+Copyright © 2022, Office for National Statistics (https://www.ons.gov.uk)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package handler
+
+import (
+	"net/http"
+	// ctrl "sigs.k8s.io/controller-runtime"
+)
+
+// Error represents a handler error. It provides methods for a HTTP status
+// code and embeds the built-in error interface.
+type Error interface {
+	error
+	Status() int
+}
+
+// StatusError represents an error with an associated HTTP status code
+type StatusError struct {
+	Code int
+	Err  error
+}
+
+// Error satisfies the error interface
+func (se StatusError) Error() string {
+	return se.Err.Error()
+}
+
+// Status returns our HTTP status code
+func (se StatusError) Status() int {
+	return se.Code
+}
+
+// Env for a handler to accept
+type Env interface{}
+
+// Handler accepts environment input and includes our handler signature
+type Handler struct {
+	Env
+	Func func(e Env, w http.ResponseWriter, r *http.Request) error
+}
+
+// ServeHTTP satisfies the http.Handler
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h.Func(h.Env, w, r)
+	if err != nil {
+		switch e := err.(type) {
+		case Error:
+			http.Error(w, e.Error(), e.Status())
+		default:
+			// Any error types we don't specifically look out for default
+			// to serving a HTTP 500
+			http.Error(w, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+		}
+	}
+}
